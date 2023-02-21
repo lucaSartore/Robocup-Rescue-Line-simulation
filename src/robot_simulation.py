45f95c9176150fd19cf9b6161fe14d7bc3a252ed
lucaSartore <lucasar.com8@gmail.com>
2023-02-21 13:14:43 +0100

    Make stuff private

--- src/robot_simulation.py
+++ src/robot_simulation.py
@@ -38,73 +38,81 @@ class Robot:
                  start_pos_x: float = DEFAULT_START_POS_X,
                  start_pos_y: float = DEFAULT_START_POS_Y,
                  start_angle: float = DEFAULT_START_ANGLE,
-                 _max_speed: float = DEFAULT_MAX_SPEED,
-                 _ppi: int = DEFAULT_PPI,
-                 _camera_x_dimension: float = DEFAULT_CAMERA_X_DIMENSION,
-                 _camera_y_dimension: float = DEFAULT_CAMERA_Y_DIMENSION,
-                 _camera_x_offset: float = DEFAULT_CAMERA_X_OFFSET,
-                 _robot_wight: float = DEFAULT_ROBOT_WIGHT
+                 max_speed: float = DEFAULT_MAX_SPEED,
+                 ppi: int = DEFAULT_PPI,
+                 camera_x_dimension: float = DEFAULT_CAMERA_X_DIMENSION,
+                 camera_y_dimension: float = DEFAULT_CAMERA_Y_DIMENSION,
+                 camera_x_offset: float = DEFAULT_CAMERA_X_OFFSET,
+                 robot_wight: float = DEFAULT_ROBOT_WIGHT
                  ):
 
         # x position in cm of the robot
-        self.pos_x: float = start_pos_x
+        self.__pos_x: float = start_pos_x
         # y position in cm of the robot
-        self.pos_y: float = start_pos_y
+        self.__pos_y: float = start_pos_y
         # x angle of the robot orientation (in radians)
-        self.angle: float = start_angle
+        self.__angle: float = start_angle
         # maximum speed of the robot in cm/s
-        self.max_speed: float = _max_speed
+        self.__max_speed: float = max_speed
         # pixel per inch of the map the robot has memorized
-        self.ppi: float = _ppi
+        self.__ppi: float = ppi
         # the map the robot has memorized
-        self.map = cv2.imread(map_path)
+        self.__map = cv2.imread(map_path)
         # the dimension in cm of the horizontal camera view
-        self.camera_x_dimension: float = _camera_x_dimension
+        self.__camera_x_dimension: float = camera_x_dimension
         # the dimension in cm of the vertical camera view
-        self.camera_y_dimension: float = _camera_y_dimension
+        self.__camera_y_dimension: float = camera_y_dimension
         # the offset the camera has from the center of the robot in cm
-        self.camera_x_offset: float = _camera_x_offset
+        self.__camera_x_offset: float = camera_x_offset
         # the wight of the robot
-        self.robot_wight: float = _robot_wight
+        self.__robot_wight: float = robot_wight
         # relative speed (from -255 to 255) of the left motor
-        self.speed_left: int = 0
+        self.__speed_left: int = 0
         # relative speed (from -255 to 255) of the right motor
-        self.speed_right: int = 0
+        self.__speed_right: int = 0
         # a boolean flag used to stopp the updater thread
-        self.thread_running: bool = True
+        self.__thread_running: bool = True
         # launching the thread that update the position
-        self.updater_thread = Thread(target=self.position_updater, args=())
-        self.updater_thread.start()
+        self.__updater_thread = Thread(target=self.__position_updater, args=())
+        self.__updater_thread.start()
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return f"Robot at X:{self.pos_x} Y:{self.pos_y} angle:{self.angle}"
+        return f"Robot at X:{self.__pos_x} Y:{self.__pos_y} angle:{self.__angle}"
 
     # disable the thread when finish using it
     def __del__(self):
-        self.thread_running = False
+        self.__thread_running = False
 
     # the thread that constantly update the position of the robot
-    def position_updater(self):
-        while self.thread_running:
+    def __position_updater(self):
+        while self.__thread_running:
 
-            self.pos_x += self.speed_left
-            self.pos_y += self.speed_right
+            self.__pos_x += self.__speed_left
+            self.__pos_y += self.__speed_right
 
             time.sleep(0.1)
+
+    def set_motors_speeds(self, right: int, left: int):
+        assert -255 <= right <= 255 and -255 <= left <= 255, "the speeds of the motor MUST be in the -255 to 255 range"
+        self.__speed_right = right
+        self.__speed_left = left
 
 
 def test():
 
     r = Robot("../maps/map_1.png")
 
-    r.speed_right = 1
-    r.speed_left = -2
+    r.set_motors_speeds(100, 236)
 
     for i in range(10):
         print(r)
         time.sleep(0.2)
 
+
+    r.__del__()
 test()
+
+
